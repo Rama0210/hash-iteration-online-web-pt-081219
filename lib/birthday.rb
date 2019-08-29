1fa7848769a8a3ed1ae5e6584b4f1f54f83a3cf8
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-29 01:41:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,8 @@ def happy_birthday(birthday_kids)
  birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
+end
+
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age < 12
@@ -16,7 +18,7 @@ else
     puts  "You are too old for this."
 end
 end
-
+end
 
 
 
